5789eaa338fe9369392d9255d1184816b941708e
Morphan1 <morphanone@gmail.com>
2016-06-10 19:01:07 -0400

    Update to 1.10
    mcmonkey build Builder pls

--- src/main/java/net/jrbudda/builder/Util.java
+++ src/main/java/net/jrbudda/builder/Util.java
@@ -2,7 +2,7 @@ package net.jrbudda.builder;
 
 import net.citizensnpcs.api.jnbt.*;
 import net.jrbudda.builder.Builder.supplymap;
-import net.minecraft.server.v1_9_R2.*;
+import net.minecraft.server.v1_10_R1.*;
 import org.bukkit.ChatColor;
 import org.bukkit.material.MaterialData;
 
